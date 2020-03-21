76248855777fb48d46830c3221abf266d98778de
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-03-21 08:03:27 +0100

    Created WorkDetail Model, Migration and FActory

---
+++ database/factories/WorkDetailFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\WorkDetail;
+use Faker\Generator as Faker;
+
+$factory->define(WorkDetail::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_03_20_224546_create_work_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('beneficiary_id');
+            $table->date('date_of_appointment')->nullable();
+            $table->string('place_of_appointment')->nullable();
+            $table->unsignedBigInteger('mda_id');
+            $table->date('confirmed')->nullable();
+            $table->date('last_promotion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_details');
+    }
+}
