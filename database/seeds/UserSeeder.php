f7a15347ae0815ddc15ed2c94a258a33639fd81b
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-03-22 23:37:16 +0100

    Updated User Seeder to also Seed Roles & Permissions

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -3,6 +3,9 @@
 use App\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
 
 class UserSeeder extends Seeder
 {
@@ -13,7 +16,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class)->create([
+        $user = factory(User::class)->create([
             'first_name' => 'John',
             'last_name' => 'Doe',
             'email' => 'john@payroll.com',
@@ -22,5 +25,49 @@ class UserSeeder extends Seeder
             'domain_id' => 1,
             'remember_token' => Str::random(10),
         ]);
+
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        //Roles
+        $super_admin = Role::create(['name' => 'super_admin']);
+        $admin = Role::create(['name' => 'admin']);
+        $hod = Role::create(['name' => 'hod']);
+        $payroll_officer = Role::create(['name' => 'payroll_officer']);
+        $mfb = Role::create(['name' => 'mfb']);
+        $beneficiary = Role::create(['name' => 'beneficiary']);
+
+        //Permissions
+        $permissions = [
+            'view_beneficiaries',
+            'create_beneficiaries',
+            'edit_beneficiaries',
+            'create_allowance',
+            'create_deduction',
+            'assign_allowance',
+            'assign_deduction',
+            'update_salary_structure',
+            'approve_updates',
+            'view_pay_slip',
+            'view_mfb_schedule',
+            'download_mfb_schedule',
+            'view_pay_schedule'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $admin->givePermissionTo('view_beneficiaries', 'create_beneficiaries', 'edit_beneficiaries',
+            'create_allowance', 'create_deduction', 'assign_allowance', 'assign_deduction', 'update_salary_structure',
+            'view_pay_slip', 'view_mfb_schedule', 'download_mfb_schedule', 'view_pay_schedule');
+
+        $hod->givePermissionTo($permissions);
+
+        $payroll_officer->givePermissionTo('view_beneficiaries', 'view_pay_schedule');
+
+        $mfb->givePermissionTo('view_mfb_schedule', 'download_mfb_schedule');
+
+        $user->assignRole('super_admin');
     }
 }
