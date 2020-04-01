c58a92ae8f56bb2dad0f1cd1a4f86c818adf2d6a
Nnaji <n.richard@genesystechhub.com>
2020-04-01 15:51:53 +0100

    manage deduction

--- app/Http/Controllers/DeductionsController.php
+++ app/Http/Controllers/DeductionsController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+
+use App\Deduction;
+use App\Beneficiary;
+use Inertia\Inertia;
+use Inertia\Response;
+use App\StructuredSalary;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class DeductionsController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +26,25 @@ class DeductionsController extends Controller
      */
     public function index()
     {
-        //
+        $filters = Request::all('search');
+        $deductions = Auth::user()->domain
+        ->deductions()
+        ->with($this->relationships())
+        ->filters(Request::only('search'))
+        ->paginate()
+        ->transform(fn (Deduction $deductions) => [
+            'id' => $deductions->id,
+            'name' => $deductions->name,
+            'valuable_type' => $deductions->valuable_type,
+            'code' => $deductions->code(),
+            'domain' => $deductions->domain(),
+            'amount' => $deductions->amount(),
+            
+        ]);
+        return Inertia::render('Deductions/Index', [
+            'filters' => $filters,
+            'deductions' => $deductions,
+        ]);
     }
 
     /**
