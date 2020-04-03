4cb8c5b7f82ad1dd19fbd5dbe7d54b605e0ba3df
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-04-03 19:33:57 +0100

    Created Payroll Model, Migration and Controller

---
+++ database/migrations/2020_04_03_181141_create_payrolls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayrollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedSmallInteger('month');
+            $table->unsignedBigInteger('year');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('domain_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payrolls');
+    }
+}
