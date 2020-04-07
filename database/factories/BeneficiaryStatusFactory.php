d82d84d0c982985aee8fdf4a589489b47c5dbbc5
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-04-07 14:36:09 +0100

    Refactored Beneficiary Status to Separate Model So as to Reduce Activities on the Beneficiary Model and also Keep History of All Status Change

---
+++ database/factories/BeneficiaryStatusFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\BeneficiaryStatus;
+use Faker\Generator as Faker;
+
+$factory->define(BeneficiaryStatus::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
