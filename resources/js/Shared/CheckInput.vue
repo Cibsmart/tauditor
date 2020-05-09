e2ddd80769729bb11ad2c53f058f54b311454ffc
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-05-10 00:11:08 +0100

    Created a CheckInput Component

---
+++ resources/js/Shared/CheckInput.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="flex items-center">
+        <span v-model="checked" :id="id" role="checkbox" tabindex="0" aria-checked="false" ref="input"
+              :class="checked ? 'bg-indigo-600' : 'bg-gray-200'" @click="checked = ! checked"
+              class="relative inline-block flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline">
+            <span aria-hidden="true"
+                  :class="checked ? 'translate-x-5' : 'translate-x-0'"
+                  class="translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200"></span>
+        </span>
+
+        <label v-if="label" :for="id" class="ml-4 block select-none text-gray-800">
+            {{ label }}
+        </label>
+    </div>
+</template>
+
+<script>
+
+    export default{
+        inheritAttrs: false,
+        props: {
+            label: String,
+            id: { type: String, default() {
+                    return `check-input-${this._uid}` },
+            },
+        },
+
+        data(){
+            return{
+                checked: false
+            }
+        },
+
+        watch: {
+            checked(checked) {
+                this.$emit('input', checked);
+            }
+        },
+    }
+</script>
