0ff36e5d8a130954b171ed3c373ce05f8f7361ac
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-05-11 07:03:53 +0100

    Instantiated Vuex Store and added the Store Object to the Root Vue Instance

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,43 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import VueMeta from 'vue-meta'
 import PortalVue from 'portal-vue'
-import { InertiaApp } from '@inertiajs/inertia-vue'
+import {InertiaApp} from '@inertiajs/inertia-vue'
 
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-Vue.mixin({ methods: { route: window.route } }) //Make route helper available on client side
+Vue.mixin({methods: {route: window.route}}) //Make route helper available on client side
 Vue.config.productionTip = false //Turns off warning during development
 Vue.use(InertiaApp)
 Vue.use(PortalVue)
 Vue.use(VueMeta)
+Vue.use(Vuex)
 
 const app = document.getElementById('app')
 
-new Vue({
-  metaInfo:{
-    titleTemplate: (title) => title ? `${title} - HRMEdge` : 'HRMEdge'
-  },
-
-  render: h => h(InertiaApp, {
-    props: {
-      initialPage: JSON.parse(app.dataset.page),
-      resolveComponent: name => import(`@/Pages/${name}`).then(module => module.default),
+const store = new Vuex.Store({
+    state: {
+        basic_information_completed: false,
     },
-  }),
+    mutations: {
+        setBasicInformationCompleted(state){
+            state.basic_information_completed = true;
+        }
+    }
+})
+
+new Vue({
+    store: store,
+
+    metaInfo: {
+        titleTemplate: (title) => title ? `${title} - HRMEdge` : 'HRMEdge'
+    },
+
+    render: h => h(InertiaApp, {
+        props: {
+            initialPage: JSON.parse(app.dataset.page),
+            resolveComponent: name => import(`@/Pages/${name}`).then(module => module.default),
+        },
+    }),
 }).$mount(app)
