01994a9b11b08be899a35fc5b2a68026dd4876c6
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-05-26 23:05:02 +0100

    Created Beneficiaries Controller for API Endpoint

---
+++ app/Http/Controllers/Api/BeneficiaryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\AuditPaySchedule;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BeneficiaryController extends Controller
+{
+
+    public function index()
+    {
+        $schedules = AuditPaySchedule::query()->limit(10)->get();
+
+        return $schedules;
+    }
+}
