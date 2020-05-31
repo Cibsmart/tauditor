66ef0fb254dc9ab30897700d550903aebc27a506
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-05-31 22:06:33 +0100

    Completed Mapping of Fields to Resource

--- app/Http/Controllers/Api/BeneficiaryController.php
+++ app/Http/Controllers/Api/BeneficiaryController.php
@@ -5,14 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\AuditPaySchedule;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\BeneficiaryResource;
 
 class BeneficiaryController extends Controller
 {
 
     public function index()
     {
-        $schedules = AuditPaySchedule::query();
+        $schedules = AuditPaySchedule::query()->limit(10)->inRandomOrder()->get();
 
-        return $schedules;
+//        dd($schedules);
+
+        return BeneficiaryResource::collection($schedules);
     }
 }
--- app/Http/Resources/BeneficiaryResource.php
+++ app/Http/Resources/BeneficiaryResource.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use function number_format;
+use function array_key_exists;
 
 class BeneficiaryResource extends JsonResource
 {
@@ -14,6 +16,30 @@ class BeneficiaryResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'surname' => $this->beneficiary_name,
+            'first_name' => '',
+            'middle_name' => '',
+            'designation' => $this->designation,
+            'mobile_phone' => '',
+            'email' => '',
+            'monthly_gross_income' => number_format($this->gross_pay, 2, '.', ''),
+            'yearly_gross_income' => number_format($this->gross_pay * 12, 2, '.', ''),
+            'total_deductions' => number_format($this->total_deduction, 2, '.', ''),
+            'monthly_tax_paid' => array_key_exists('tax', $this->deductions)
+                ? number_format($this->deductions['tax'], 2, '.', '')
+                : '',
+            'pension' => array_key_exists('ansg_pen', $this->deductions)
+                ? number_format($this->deductions['ansg_pen'], 2, '.', '')
+                : '',
+            'nhf' => array_key_exists('nhf', $this->deductions)
+                ? number_format($this->deductions['nhf'], 2, '.', '')
+                : '',
+            'nhis' => array_key_exists('ashis', $this->deductions)
+                ? number_format($this->deductions['ashis'], 2, '.', '')
+                : '',
+            'nsitf' => '',
+            'life_assurance' => '',
+        ];
     }
 }
