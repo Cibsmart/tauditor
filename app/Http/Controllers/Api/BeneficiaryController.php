45ebf0abce2a5f379c4388cf30f0fbc20117b5a6
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-06-02 18:22:40 +0100

    Completed Index Method of Beneficiary API Endpoint

--- app/Http/Controllers/Api/BeneficiaryController.php
+++ app/Http/Controllers/Api/BeneficiaryController.php
@@ -2,20 +2,82 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Domain;
+use Carbon\Carbon;
+use App\AuditPayroll;
 use App\AuditPaySchedule;
+use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\PayloadCollection;
 use App\Http\Resources\BeneficiaryResource;
+use Symfony\Component\HttpFoundation\Response;
+use function response;
 
 class BeneficiaryController extends Controller
 {
 
-    public function index()
+    public function index($domain, $year, $month)
     {
-        $schedules = AuditPaySchedule::query()->limit(10)->inRandomOrder()->get();
+        $domain = Domain::find($domain);
 
-//        dd($schedules);
+        if (! $domain) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Domain Does not Exist'
+            ])->setStatusCode(Response::HTTP_NOT_FOUND);
+        }
 
-        return BeneficiaryResource::collection($schedules);
+        if (Str::length($year) !== 4) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid Year'
+            ])->setStatusCode(Response::HTTP_NOT_FOUND);
+        }
+
+        if (Str::length($month) !== 2) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid Month'
+            ])->setStatusCode(Response::HTTP_NOT_FOUND);
+        }
+
+        $payroll = AuditPayroll::query()
+                                ->where('domain_id', '=', $domain->id)
+                                ->where('year', '=', $year)
+                                ->where('month', '=', $month)
+                                ->first();
+
+        if (! $payroll) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Cannot Complete Request'
+            ])->setStatusCode(Response::HTTP_NOT_FOUND);
+        }
+
+        $schedules = AuditPaySchedule::schedules($payroll)->limit(2)->get();
+
+        if ($schedules->count() < 1) {
+            return response()->json([
+                'status' => true,
+                'message' => 'No Record Found'
+            ])->setStatusCode(Response::HTTP_NOT_FOUND);
+        }
+
+        $data = [
+            'address' => $domain->name,
+            'nigerians' => $schedules->count(),
+            'schedules' => $schedules
+        ];
+
+        return new PayloadCollection($data);
+    }
+
+    public function invalid()
+    {
+        return response()->json([
+            'status' => false,
+            'message' => 'Invalid Request'
+        ])->setStatusCode(Response::HTTP_NOT_FOUND);
     }
 }
