d90fae018704aeaeaf35f1c09eb4592f8a45a247
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-06-09 21:56:24 +0000

    Added a Destroy in AuditPayScheduleController to Clear AuditPaySchedule, AutopaySchedule, and AuditReport

---
+++ database/migrations/2020_06_08_111243_add_columns_to_audit_sub_mda_schedules.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToAuditSubMdaSchedules extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('audit_reports', function (Blueprint $table) {
+            $table->unsignedBigInteger('audit_sub_mda_schedule_id')
+                  ->after('audit_payroll_category_id')
+                  ->default(1);
+            $table->softDeletes();
+
+            $table->foreign('audit_sub_mda_schedule_id')->references('id')->on('audit_sub_mda_schedules');
+        });
+
+        Schema::table('autopay_schedules', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('audit_sub_mda_schedules', function (Blueprint $table) {
+            //
+        });
+    }
+}
