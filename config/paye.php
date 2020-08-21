767fdfa467e38503063b610cb96f5d79fa927328
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-08-21 14:23:30 +0000

    Update TMS PAYE API Configuration

--- config/paye.php
+++ config/paye.php
@@ -4,7 +4,8 @@ return [
 
     'staff' => [
         'url' => env('APP_MART_URL'),
-        'secret' => env('APP_SECRET'),
+        'id' => env('APP_MART_ID'),
+        'secret' => env('APP_MART_SECRET'),
         'project_id' => env('PROJECT_ID_STAFF'),
         'project_name' => env('PROJECT_NAME_STAFF'),
         'project_category' => env('PROJECT_CATEGORY'),
@@ -12,7 +13,8 @@ return [
 
     'lgea' => [
         'url' => env('APP_MART_URL'),
-        'secret' => env('APP_SECRET'),
+        'id' => env('APP_MART_ID'),
+        'secret' => env('APP_MART_SECRET'),
         'project_id' => env('PROJECT_ID_LGEA'),
         'project_name' => env('PROJECT_NAME_LGEA'),
         'project_category' => env('PROJECT_CATEGORY'),
@@ -20,7 +22,8 @@ return [
 
     'lgsc' => [
         'url' => env('APP_MART_URL'),
-        'secret' => env('APP_SECRET'),
+        'id' => env('APP_MART_ID'),
+        'secret' => env('APP_MART_SECRET'),
         'project_id' => env('PROJECT_ID_LGSC'),
         'project_name' => env('PROJECT_NAME_LGSC'),
         'project_category' => env('PROJECT_CATEGORY'),
