35dafd6272b88c5567c723a343e133725516a772
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2020-09-22 14:19:35 +0100

    Rename LoanData to PaymentHistory

--- app/Http/Controllers/Api/PaymentHistoryController.php
+++ app/Http/Controllers/Api/PaymentHistoryController.php
@@ -1,28 +1,32 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
-use App\Domain;
-use App\AuditPaySchedule;
+use App\Models\Domain;
 use Illuminate\Support\Str;
-use App\Http\Resources\LoanResource;
+use Illuminate\Http\Request;
+use App\Models\AuditPaySchedule;
+use App\Http\Controllers\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use App\Http\Resources\LoanResourceCollection;
 use function response;
-use function number_format;
 
-class LoanDataController extends Controller
+class PaymentHistoryController extends Controller
 {
     //
-    public function index($domain, $verification_number, $account_number)
+    public function index(Request $request)
     {
+        $domain = $request->domain;
+        $verification_number = $request->staff_id;
+        $account_number = $request->account_number;
+
         $domain = Domain::find(Str::of($domain)->trim());
 
         if (! $domain) {
             return response()->json([
-                'is_staff_valid'  => false,
-                'data'  => [],
-                'message' => 'Invalid Domain',
+                'is_staff_valid' => false,
+                'data'           => [],
+                'message'        => 'Invalid Domain',
             ])->setStatusCode(Response::HTTP_NOT_FOUND);
         }
 
@@ -33,9 +37,9 @@ class LoanDataController extends Controller
 
         if (! $beneficiary) {
             return response()->json([
-                'is_staff_valid'  => false,
-                'data'  => [],
-                'message' => 'Staff ID Does not Exist',
+                'is_staff_valid' => false,
+                'data'           => [],
+                'message'        => 'Staff ID Does not Exist',
             ])->setStatusCode(Response::HTTP_NOT_FOUND);
         }
 
@@ -46,9 +50,9 @@ class LoanDataController extends Controller
 
         if (! $account) {
             return response()->json([
-                'is_staff_valid'  => false,
-                'data'  => [],
-                'message' => 'Account Number Does not Exist',
+                'is_staff_valid' => false,
+                'data'           => [],
+                'message'        => 'Account Number Does not Exist',
             ])->setStatusCode(Response::HTTP_NOT_FOUND);
         }
 
@@ -56,16 +60,15 @@ class LoanDataController extends Controller
         $schedules = AuditPaySchedule::allSchedules()
                                      ->orderByMonth()
                                      ->where('verification_number', Str::of($verification_number)->trim())
-                                     ->where('account_number', Str::of($account_number)->trim())
                                      ->where('domain_id', Str::of($domain->id)->trim())
                                      ->limit(3)
                                      ->get();
 
         if ($schedules->isEmpty()) {
             return response()->json([
-                'is_staff_valid'  => false,
-                'data'  => [],
-                'message' => 'No Matching Record',
+                'is_staff_valid' => false,
+                'data'           => [],
+                'message'        => 'No Matching Record',
             ])->setStatusCode(Response::HTTP_NOT_FOUND);
         }
 
@@ -75,20 +78,29 @@ class LoanDataController extends Controller
         $mda_name = $mda_name === $sub_name ? $mda_name : "{$mda_name} ({$sub_name})";
 
         $data = [
-            'staff_id' => $schedules[0]->verification_number,
-            'staff_name' => $schedules[0]->beneficiary_name,
-            'staff_cadre' => $schedules[0]->beneficiary_cadre,
-            'staff_mda' => $mda_name,
-            'bank_name' => $schedules[0]->bank_name,
+            'staff_id'       => $schedules[0]->verification_number,
+            'staff_name'     => $schedules[0]->beneficiary_name,
+            'staff_cadre'    => $schedules[0]->beneficiary_cadre,
+            'staff_mda'      => $mda_name,
+            'bank_name'      => $schedules[0]->bank_name,
             'account_number' => $schedules[0]->account_number,
-            'schedules' => $schedules->map(fn ($schedule) => [
-                'net_pay' => number_format($schedule->net_pay, 2, '.', ','),
-                'month' => "{$schedule->month_name} {$schedule->year}",
-                'bank_name' => $schedule->bank_name,
+            'schedules'      => $schedules->map(fn ($schedule) => [
+                'net_pay'        => number_format($schedule->net_pay, 2, '.', ','),
+                'month'          => "{$schedule->month_name} {$schedule->year}",
+                'bank_name'      => $schedule->bank_name,
                 'account_number' => $schedule->account_number,
             ]),
         ];
 
         return new LoanResourceCollection($data);
+    }
+
+    public function invalid()
+    {
+        return response()->json([
+            'is_staff_valid' => false,
+            'data'           => [],
+            'message'        => 'Invalid Request',
+        ])->setStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 }
