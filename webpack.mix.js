2a5cbb8e4246db5d9723de84fbc544e3e5dae867
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2021-05-09 05:51:20 +0100

    Updated Composer Dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,22 +1,18 @@
 const path = require('path');
 const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss')
-const cssImport = require('postcss-import')
-const cssNesting = require('postcss-nesting')
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css')
-    .options({
-        postCss: [
-            cssImport(),
-            cssNesting(),
-            tailwindcss('tailwind.config.js'),
-        ]
-    })
+    .vue()
+    .postCss('resources/css/app.css', 'public/css' , [
+        require('postcss-import'),
+        require('postcss-nesting'),
+        require('tailwindcss'),
+    ])
     .webpackConfig({
         output: {chunkFilename: 'js/[name].js?id=[chunkhash]'},
         resolve: {
             alias: {
+                vue$: 'vue/dist/vue.runtime.esm.js',
                 '@': path.resolve('resources/js'),
             },
         },
