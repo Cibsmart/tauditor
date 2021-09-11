f1a257e72210602858260a66c3e9f6dff8c9ae11
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2021-09-11 16:30:09 +0100

    Implemented show and store methods for loan mandates

--- app/Http/Controllers/FidelityMandateController.php
+++ app/Http/Controllers/FidelityMandateController.php
@@ -14,15 +14,15 @@ class FidelityMandateController extends Controller
         $this->middleware('auth');
     }
 
-    public function mandate()
+    public function index()
     {
-        $filters = Request::all('search', 'status');
+        $filters = Request::all('search', 'status', 'processed');
 
         $mandates = LoanMandate::query()
                                ->orderBy('status')
-                               ->filter(Request::only('search', 'status'))
+                               ->filter(Request::only('search', 'status', 'processed'))
                                ->paginate()
-                               ->transform(fn ($m) => [
+                               ->transform(fn($m) => [
                                    'id'                      => $m->id,
                                    'name'                    => $m->beneficiary->name,
                                    'verification_number'     => $m->staff_id,
@@ -37,12 +37,49 @@ class FidelityMandateController extends Controller
                                    'status'                  => $m->loan_status->name,
                                    'color'                   => $m->color,
                                    'date_disbursed'          => $m->formatted_disbursement_date,
+                                   'processed'               => $m->processed,
                                ]);
 
-        return Inertia::render('Fidelity/Mandate', [
-            'filters'   => $filters,
+        return Inertia::render('Fidelity/Index', [
+            'filters'  => $filters,
             'mandates' => $mandates,
-            'statuses' => LoanStatus::all()
+            'statuses' => LoanStatus::all(),
         ]);
+    }
+
+    public function show(LoanMandate $mandate)
+    {
+        return Inertia::render('Fidelity/Show', [
+            'mandate' => [
+                'id'                      => $mandate->id,
+                'name'                    => $mandate->beneficiary->name,
+                'verification_number'     => $mandate->staff_id,
+                'bvn'                     => $mandate->bvn,
+                'account_number'          => $mandate->account_number,
+                'reference'               => $mandate->reference,
+                'loan_amount'             => $mandate->formatted_loan_amount,
+                'collection_amount'       => $mandate->formatted_collection_amount,
+                'total_collection_amount' => $mandate->total_collection_amount,
+                'number_of_repayments'    => $mandate->number_of_repayments,
+                'number_repaid'           => 0,
+                'status'                  => $mandate->loan_status->name,
+                'color'                   => $mandate->color,
+                'date_disbursed'          => $mandate->formatted_disbursement_date,
+                'processed'               => $mandate->processed,
+            ],
+        ]);
+    }
+
+    public function store()
+    {
+        $validated = request()->validate([
+            'mandate_id' => 'required|numeric',
+        ]);
+
+        $mandate = LoanMandate::find($validated['mandate_id']);
+
+        $mandate->process();
+
+        return back(302)->with('success', 'Loan Mandate has been marked as processed');
     }
 }
