dfc1b68cb2f768ef3dc1a633ae263b46ad0fee7b
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2021-09-22 13:35:50 +0100

    Completed Implementation of Send Deduction Confirmation Action

--- app/Actions/SendDeductionConfirmation.php
+++ app/Actions/SendDeductionConfirmation.php
@@ -2,41 +2,48 @@
 
 namespace App\Actions;
 
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Http;
+use App\Models\FidelityLoanSchedule;
 use Illuminate\Support\Facades\Cache;
+use App\Models\FidelityLoanDeduction;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class SendDeductionConfirmation
 {
     use AsAction;
 
-    public function handle()
+    public function handle(FidelityLoanSchedule $schedule)
     {
         $tokenUrl = config('fidelity.generate_token_url');
         $clientId = config('fidelity.client_id');
         $clientSecret = config('fidelity.client_secret');
 
-        //Get Fidelity Loan Access Token and cache the token for 3600 seconds
+        //Get Fidelity Loan Access Token and cache the token for 3000 seconds (50 mins)
         $token = Cache::remember(
             'fidelity_loan_token',
-            3600,
+            3000,
             fn () => Http::post($tokenUrl, [
             'ClientID' => $clientId,
             'ClientSecret' => $clientSecret,
             ])->json()['access_token']
         );
 
-        dd($token);
-
-        //TODO: Implement Send Deduction Confirmation
-        //Get and Prepare Payload to be send in bulk to Fidelity
         $bulkUrl = config('fidelity.confirm_deduction_url_bulk');
 
-        $narration = '';
-        $date = '';
-        $total = '';
-        $data = '';
+        $narration = $schedule->narration;
 
+        $timestamp = Str::afterLast($narration, '|');
+        $date = Carbon::createFromTimestamp($timestamp)
+                      ->setDay(25)
+                      ->setTime(23, 00)
+                      ->format('Y-m-d\TH:i:s+000Z');
+        $total = $schedule->totalAmount();
+        $data = $schedule->deductions->transform(fn (FidelityLoanDeduction $deduction) => [
+            'LoanAccount' => $deduction->loan_account,
+            'Amount' => $deduction->amount
+        ]);
 
         $response = Http::withToken($token)
                         ->post($bulkUrl, [
@@ -45,5 +52,12 @@ class SendDeductionConfirmation
                             'TotalAmount' => $total,
                             'data' => $data
                         ]);
+
+        $payload = $response->json();
+
+        if ($response->successful() && $payload['Status'] === 'SUCCESS') {
+            $schedule->confirmation_sent = now();
+            $schedule->save();
+        }
     }
 }
