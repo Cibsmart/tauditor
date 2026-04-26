b3916b4c1221c4cdceb4bb40a0e2ad3781aa991a
Barnabas Ifebude <b.ifebude@genesystechhub.com>
2026-04-26 22:57:01 +0100

    Refactored `app.js` to replace eager page imports with `resolvePageComponent` for dynamic resolution and improved maintainability.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,21 @@
 import { createInertiaApp } from '@inertiajs/vue3';
 import { createHead } from '@vueuse/head';
-
 import axios from 'axios';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
-
 createInertiaApp({
   title: (title) => (title ? `${title} - TAuditor` : 'TAuditor'),
-  resolve: (name) => pages[`./Pages/${name}.vue`],
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.vue`,
+      import.meta.glob('./Pages/**/*.vue'),
+    ),
   setup({ el, App, props, plugin }) {
     const head = createHead();
     createApp({ render: () => h(App, props) })
